3a0d7acd302d8efcb4c240772d282b0b9871d4a1
xVorttex <72511773+xVorttex@users.noreply.github.com>
2021-08-29 18:04:15 +0300

    init

---
+++ src/eu/vortexgg/mmorpg/util/HoloItem.java
@@ -0,0 +1,20 @@
+package eu.vortexgg.mmorpg.util;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.inventory.ItemStack;
+
+public class HoloItem {
+
+    public static ArmorStand createHoloItem(Material material, Location loc) {
+	ArmorStand armorStand = (ArmorStand)loc.getWorld().spawn(loc, ArmorStand.class);
+        armorStand.setVisible(false);
+        armorStand.setHelmet(new ItemStack(material));
+        armorStand.setGravity(false);
+        armorStand.setInvulnerable(true);
+        armorStand.setSmall(true);
+        return armorStand;
+    }
+
+}
---
+++ src/eu/vortexgg/mmorpg/util/VectorUtil.java
@@ -0,0 +1,17 @@
+package eu.vortexgg.mmorpg.util;
+
+import org.bukkit.util.Vector;
+
+public class VectorUtil {
+    public static Vector a(Vector paramVector, double paramDouble) {
+	paramDouble = Math.toRadians(paramDouble);
+	final double cos = Math.cos(paramDouble), sin = Math.sin(paramDouble), y = paramVector.getY() * cos - paramVector.getZ() * sin, z = paramVector.getY() * sin + paramVector.getZ() * cos;
+	return paramVector.setY(y).setZ(z);
+    }
+
+    public static Vector b(Vector paramVector, double paramDouble) {
+	paramDouble = Math.toRadians(paramDouble);
+	final double cos = Math.cos(paramDouble), sin = Math.sin(paramDouble), x = paramVector.getX() * cos + paramVector.getZ() * sin, z = paramVector.getX() * -sin + paramVector.getZ() * cos;
+	return paramVector.setX(x).setZ(z);
+    }
+}
